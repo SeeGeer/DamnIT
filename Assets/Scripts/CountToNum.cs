9825521c1dba0ea507d77c2f7fb00a6b9898ac95
SeeGeer <32983446+SeeGeer@users.noreply.github.com>
2018-07-19 22:04:56 -0400

    tickets increase animation and day transfer
    tickets increase animation and day transfer

---
+++ Assets/Scripts/CountToNum.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class CountToNum : MonoBehaviour {
+
+    public Text CountDisplay;
+    public Text ticketDisplay;
+    public int TargetNum;
+    public GameObject CloseButton;
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(CountToTarget());
+        
+        
+    }
+
+    private void Update()
+    {
+
+        CountDisplay.text = "" + DBManager.ticketNum;
+        ticketDisplay.text = "" + DBManager.ticketNum;
+    }
+
+    IEnumerator CountToTarget()
+    {
+        
+        while (TargetNum > DBManager.ticketNum)
+        {
+            yield return new WaitForSeconds(0.06f);
+            DBManager.ticketNum ++;
+        }
+
+        CloseButton.SetActive(true);
+
+
+
+
+    }
+}
