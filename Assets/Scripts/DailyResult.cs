b9548a68d23982f424f4d8a0b754f3872a49e866
SeeGeer <32983446+SeeGeer@users.noreply.github.com>
2018-07-25 00:26:26 -0400

    final_version

--- Assets/Scripts/DailyResult.cs
+++ Assets/Scripts/DailyResult.cs
@@ -20,7 +20,7 @@ public class DailyResult : MonoBehaviour {
         rNumber = Random.Range(12, 33);
         resultDisplay.text = "Here is your day " + day + " result \n \n" +
             "- Your rating is XXX \n" +
-            "- You have finished "+ rNumber + " tickets \n" +
+            "- You have finished 29 tickets \n" +
             "- You are ranking #3 in your group \n \n \n" +
 
             "Leaderboard:\n" +
